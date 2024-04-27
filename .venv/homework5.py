cfd432416cb766461cd55431547302da0b2a3247
EvilCrake <48925014+EvilCrake@users.noreply.github.com>
2024-04-27 19:06:47 +0300

    Homework5

---
+++ .venv/homework5.py
@@ -0,0 +1,15 @@
+#Работа со списками
+my_list = ['apple', 'banana', 'cherry', 'peach', 'strawberry', 'pear']
+print(type(my_list))
+print(my_list)
+print(my_list[0], my_list[5])
+print(my_list[2:5])
+my_list[3] = 'orange'
+print(my_list)
+#Работа со словарями
+my_dict={'HI': 'Привет', 'How': 'Как', 'Processing': 'Обработка', 'Popularity':'Популярность'}
+print(type(my_dict))
+print(my_dict)
+print(my_dict['HI'])
+my_dict.setdefault('Hello', 'Здраствуйте')
+print(my_dict)
